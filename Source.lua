f614f8f87e65f93a693f8535e5d98ecd38ae0fcf
ewsosolol <136973288+ewsosolol@users.noreply.github.com>
2023-09-14 15:57:54 +0300

    Add files via upload

---
+++ Source.lua
@@ -0,0 +1,65 @@
+--[[ _______                ________                   __ __            __    __          __      
+  ______                    __            __           ______                         _______           __       __                   
+ /      \                  |  \          |  \         /      \                       |       \         |  \     |  \                  
+|  ▓▓▓▓▓▓\ _______  ______  \▓▓ ______  _| ▓▓_       |  ▓▓▓▓▓▓\ ______   ______      | ▓▓▓▓▓▓▓\ ______ | ▓▓____ | ▓▓ ______  __    __ 
+| ▓▓___\▓▓/       \/      \|  \/      \|   ▓▓ \      | ▓▓_  \▓▓/      \ /      \     | ▓▓__| ▓▓/      \| ▓▓    \| ▓▓/      \|  \  /  \
+ \▓▓    \|  ▓▓▓▓▓▓▓  ▓▓▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\\▓▓▓▓▓▓      | ▓▓ \   |  ▓▓▓▓▓▓\  ▓▓▓▓▓▓\    | ▓▓    ▓▓  ▓▓▓▓▓▓\ ▓▓▓▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\\▓▓\/  ▓▓
+ _\▓▓▓▓▓▓\ ▓▓     | ▓▓   \▓▓ ▓▓ ▓▓  | ▓▓ | ▓▓ __     | ▓▓▓▓   | ▓▓  | ▓▓ ▓▓   \▓▓    | ▓▓▓▓▓▓▓\ ▓▓  | ▓▓ ▓▓  | ▓▓ ▓▓ ▓▓  | ▓▓ >▓▓  ▓▓ 
+|  \__| ▓▓ ▓▓_____| ▓▓     | ▓▓ ▓▓__/ ▓▓ | ▓▓|  \    | ▓▓     | ▓▓__/ ▓▓ ▓▓          | ▓▓  | ▓▓ ▓▓__/ ▓▓ ▓▓__/ ▓▓ ▓▓ ▓▓__/ ▓▓/  ▓▓▓▓\ 
+ \▓▓    ▓▓\▓▓     \ ▓▓     | ▓▓ ▓▓    ▓▓  \▓▓  ▓▓    | ▓▓      \▓▓    ▓▓ ▓▓          | ▓▓  | ▓▓\▓▓    ▓▓ ▓▓    ▓▓ ▓▓\▓▓    ▓▓  ▓▓ \▓▓\
+  \▓▓▓▓▓▓  \▓▓▓▓▓▓▓\▓▓      \▓▓ ▓▓▓▓▓▓▓    \▓▓▓▓      \▓▓       \▓▓▓▓▓▓ \▓▓           \▓▓   \▓▓ \▓▓▓▓▓▓ \▓▓▓▓▓▓▓ \▓▓ \▓▓▓▓▓▓ \▓▓   \▓▓
+                              | ▓▓                                                                                                    
+                              | ▓▓                                                                                                    
+                               \▓▓                                                                                                    
+                               
+                              ]]--
+--[[ _______                ________                   __ __            __    __          __      
+  ______                    __            __           ______                         _______           __       __                   
+ /      \                  |  \          |  \         /      \                       |       \         |  \     |  \                  
+|  ▓▓▓▓▓▓\ _______  ______  \▓▓ ______  _| ▓▓_       |  ▓▓▓▓▓▓\ ______   ______      | ▓▓▓▓▓▓▓\ ______ | ▓▓____ | ▓▓ ______  __    __ 
+| ▓▓___\▓▓/       \/      \|  \/      \|   ▓▓ \      | ▓▓_  \▓▓/      \ /      \     | ▓▓__| ▓▓/      \| ▓▓    \| ▓▓/      \|  \  /  \
+ \▓▓    \|  ▓▓▓▓▓▓▓  ▓▓▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\\▓▓▓▓▓▓      | ▓▓ \   |  ▓▓▓▓▓▓\  ▓▓▓▓▓▓\    | ▓▓    ▓▓  ▓▓▓▓▓▓\ ▓▓▓▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\\▓▓\/  ▓▓
+ _\▓▓▓▓▓▓\ ▓▓     | ▓▓   \▓▓ ▓▓ ▓▓  | ▓▓ | ▓▓ __     | ▓▓▓▓   | ▓▓  | ▓▓ ▓▓   \▓▓    | ▓▓▓▓▓▓▓\ ▓▓  | ▓▓ ▓▓  | ▓▓ ▓▓ ▓▓  | ▓▓ >▓▓  ▓▓ 
+|  \__| ▓▓ ▓▓_____| ▓▓     | ▓▓ ▓▓__/ ▓▓ | ▓▓|  \    | ▓▓     | ▓▓__/ ▓▓ ▓▓          | ▓▓  | ▓▓ ▓▓__/ ▓▓ ▓▓__/ ▓▓ ▓▓ ▓▓__/ ▓▓/  ▓▓▓▓\ 
+ \▓▓    ▓▓\▓▓     \ ▓▓     | ▓▓ ▓▓    ▓▓  \▓▓  ▓▓    | ▓▓      \▓▓    ▓▓ ▓▓          | ▓▓  | ▓▓\▓▓    ▓▓ ▓▓    ▓▓ ▓▓\▓▓    ▓▓  ▓▓ \▓▓\
+  \▓▓▓▓▓▓  \▓▓▓▓▓▓▓\▓▓      \▓▓ ▓▓▓▓▓▓▓    \▓▓▓▓      \▓▓       \▓▓▓▓▓▓ \▓▓           \▓▓   \▓▓ \▓▓▓▓▓▓ \▓▓▓▓▓▓▓ \▓▓ \▓▓▓▓▓▓ \▓▓   \▓▓
+                              | ▓▓                                                                                                    
+                              | ▓▓                                                                                                    
+                               \▓▓                                                                                                    
+                               
+                              ]]--
+if game.PlaceId == 192800 then
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/ewsosolol/ed7aef0e47e940578010cdc6e26aea03/raw/eb74b2272a3ab77ef50b3dc58ac0cb33d4293db6/PIZZA%2520Place%2520script%2520new')))()
+end;
+
+if game.PlaceId == 2971329387 then
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/ewsosolol/f518856ee404788b8d10544791fe9c54/raw/8045f46edd97a273baf2a7117636e23a5f659474/Cook%2520Burgers%2520Script%2520Release')))()
+end;
+
+if game.PlaceId == 11520107397 then
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/ewsosolol/8c92805e9cddc2f1f9ed90c484a8ebf0/raw/50a3dcd89d553d1a6ffa5e2eb70d78e9bbaed627/Slap%2520battles%2520hub%2520release')))()
+end;
+
+if game.PlaceId ==  6403373529 then
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/ewsosolol/265d8e73df8b066ae9f335e60d947d55/raw/679e01406b8b3ccbbd6ff4876de6c1a7c189667f/gistfile1.txt')))()
+end;
+
+if game.PlaceId == 286090429 then
+loadstring(game:HttpGet("https://gist.githubusercontent.com/ewsosolol/f85ffc144baf470fff71eec25be44670/raw/f7a86665a8e759b623c1984413b1e2d78d1be883/Arsenal%2520script", true))()
+end;
+
+if game.PlaceId == 4490140733 then
+loadstring(game:HttpGet("https://gist.githubusercontent.com/ewsosolol/fbaebd820ebfe1acb4c83cb6c4ad3590/raw/8afee3aff8503137cb09f215f901c1d5f46155e5/My%2520Restaurant", true))()
+end;
+
+if  game.PlaceId == 7346416636 then
+loadstring(game:HttpGet("https://gist.githubusercontent.com/ewsosolol/3d5aeec85434d8cb5ee5b3d37f228460/raw/304f6d1e2b24d9258baeab1af4d4dc56f3a961de/idk%2520what%2520game%2520script", true))()
+end;
+
+if  game.PlaceId == 537413528 then
+loadstring(game:HttpGet("https://gist.githubusercontent.com/ewsosolol/5460a9545c203fcf98becce2a2d59735/raw/9f9902a3b1819bed293dba8ae759d97e30887a2a/bulid%2520a%2520boat%2520script"))()
+end;
+
+if  game.PlaceId == 3101667897 then
+loadstring(game:HttpGet("https://pastebin.com/raw/b4nrYCzm"))()
+end;
